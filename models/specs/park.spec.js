c26ffd9baf58497a38325ad5b9b90b2133fcb6dd
Jos Bongers <josbongers@Willems-MacBook-Pro.local>
2022-05-06 08:41:24 +0100

    setup for testing ready

---
+++ models/specs/park.spec.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const Park = require('../park.js');
+const Dinosaur = require('../dinosaur.js');
+
+describe('Park', function () {
+
+    beforeEach(function () {
+
+    });
+
+
+    // MVP
+    xit('Add a dinosaur to its collection of dinosaurs', function () {
+
+    });
+    xit('Remove a dinosaur from its collection of dinosaurs', function () {
+
+    });
+    xit('Find the dinosaur that attracts the most visitors', function () {
+
+    });
+    xit('Find all dinosaurs of a particual species', function () {
+
+    });
+    xit('Calculate the total number of visitors per day', function () {
+
+    });
+    xit('Calculate the total number of visistors per year', function () {
+
+    });
+    xit('Calculate the total revenue from ticket sales for one year', function () {
+
+    });
+
+    // EXTENSIONS
+    xit('Remove all dinosaurs of a particular species', function () {
+
+    });
+    xit('Provide an object containing each of the diet types and the number of dinosaurs in the park of that diet type', function () {
+
+    });
+})
